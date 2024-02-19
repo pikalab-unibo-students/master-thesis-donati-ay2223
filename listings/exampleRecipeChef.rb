3a9b353d3c58bfdb4edf762669e97365a4b9e0a1
AlbertoDonati <alberto.donati6@studio.unibo.it>
2024-02-19 12:54:39 +0100

    chef improve some subsections

---
+++ listings/exampleRecipeChef.rb
@@ -0,0 +1,34 @@
+node.default['main']['doc_root'] = "/vagrant/web"
+
+execute "apt-get update" do
+ command "apt-get update"
+end
+
+apt_package "apache2" do
+ action :install
+end
+
+service "apache2" do
+ action [ :enable, :start ]
+end
+
+directory node['main']['doc_root'] do
+ owner 'www-data'
+ group 'www-data'
+ mode '0644'
+ action :create
+end
+
+cookbook_file "#{node['main']['doc_root']}/index.html" do
+ source 'index.html'
+ owner 'www-data'
+ group 'www-data'
+ action :create
+end
+
+template "/etc/apache2/sites-available/000-default.conf" do
+ source "vhost.erb"
+ variables({ :doc_root => node['main']['doc_root'] })
+ action :create
+ notifies :restart, resources(:service => "apache2")
+end
